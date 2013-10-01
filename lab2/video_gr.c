24b4ad286f164843ed31177e02898b7bb4a9f5ca
msandim <migsandim@hotmail.com>
2013-10-01 17:52:03 +0000

    - Makefile: foi retirada uma linha do makefile que estava a dar erro. - vg_init: a trabalhar mais ou menos - neste momento quando se pede para mostrar o endereço visualiza 0 (??)
    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@33 979b2d34-c887-48f1-a8b5-8c534db5e80d

--- lab2/video_gr.c
+++ lab2/video_gr.c
@@ -20,6 +20,10 @@
 #define V_RES		  768
 #define BITS_PER_PIXEL	  8
 
+/* Stuff to make the VBE call */
+#define BIT(n) (0x1 << (n))
+#define LINEAR_MODEL_BIT 14
+
 /* Private global variables */
 
 static char *video_mem;		/* Process address to which VRAM is mapped */
@@ -30,7 +34,33 @@ static unsigned bits_per_pixel; /* Number of VRAM bits per pixel */
 
 void * vg_init(unsigned short mode) {
 
-	return NULL;
+	// version with hardcoded values
+	h_res = H_RES;
+	v_res = V_RES;
+	bits_per_pixel = BITS_PER_PIXEL;
+
+	struct reg86u reg86;
+
+	reg86.u.b.intno = 0x10; /* BIOS video services */
+	reg86.u.b.ah = 0x4F;    /* Prepare to make a VBE Call */
+	reg86.u.b.al = 0x02;    /* Using function 2 */
+
+
+	/*  Bit 14 of the BX register should be set to use
+	a linear frame buffer model */
+	reg86.u.w.bx = BIT(LINEAR_MODEL_BIT) | mode; /* try to use 015 */
+	if( sys_int86(&reg86) != OK ) {
+		printf("vg_init: sys_int86() failed \n");
+		return NULL;
+	}
+
+	/* Map memory */
+
+	struct mem_range mr;
+	//video_mem = vm_map_phys(SELF, (void *)mr.mr_base, 1024*768);
+	video_mem = (char*) VRAM_PHYS_ADDR;
+
+	return video_mem;
 }
 
 int vg_fill(unsigned long color) {
@@ -46,21 +76,21 @@ long vg_get_pixel(unsigned long x, unsigned long y) {
 }
 
 int vg_draw_line(unsigned long xi, unsigned long yi, 
-		 unsigned long xf, unsigned long yf, unsigned long color) {
+		unsigned long xf, unsigned long yf, unsigned long color) {
 	return 0;
 }
 
 int vg_exit() {
-  struct reg86u reg86;
+	struct reg86u reg86;
 
-  reg86.u.b.intno = 0x10; /* BIOS video services */
+	reg86.u.b.intno = 0x10; /* BIOS video services */
 
-  reg86.u.b.ah = 0x00;    /* Set Video Mode function */
-  reg86.u.b.al = 0x03;    /* 80x25 text mode*/
+	reg86.u.b.ah = 0x00;    /* Set Video Mode function */
+	reg86.u.b.al = 0x03;    /* 80x25 text mode*/
 
-  if( sys_int86(&reg86) != OK ) {
-      printf("\tvg_exit(): sys_int86() failed \n");
-      return 1;
-  } else
-      return 0;
+	if( sys_int86(&reg86) != OK ) {
+		printf("\tvg_exit(): sys_int86() failed \n");
+		return 1;
+	} else
+		return 0;
 }
