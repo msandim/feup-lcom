6de0e8868ec7de96c173833a53edf84b56c6fc7e
msandim <migsandim@hotmail.com>
2013-10-10 23:14:31 +0000

    get_info/show_info a funcionar, utilizada get_info na square freq
    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@74 979b2d34-c887-48f1-a8b5-8c534db5e80d

--- lab3/timer.c
+++ lab3/timer.c
@@ -10,7 +10,10 @@ int hook_id;
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
 	if (timer > 2 || freq == 0) // since it's unsigned
+	{
+		printf("Error on set_square::timer.c\n");
 		return 1;
+	}
 
 	// input_to_control is what we're changing to pass as parameter to kernel call
 	unsigned char input_to_control = 0; // all at zero - 8 bits
@@ -41,10 +44,17 @@ int timer_set_square(unsigned long timer, unsigned long freq) {
 
 	input_to_control |= TIMER_SQR_WAVE; // select option 3 (square wave) in counting mode
 
-	/*
-	 * Not supposed to change LSB
-	input_to_control |= TIMER_BIN; // count in binary
-	 */
+	unsigned char timer_config;
+
+	timer_get_config(timer,&timer_config);
+
+	unsigned char bcd = timer_config & 0x1;
+
+	if (bcd == 0)
+		input_to_control |= TIMER_BIN; // count in binary
+	else
+		input_to_control |= TIMER_BCD; // count in BCD
+
 	freq_divisor = TIMER_FREQ / freq; // obtain the divisor
 
 	// divide divisor in LSB and MSB
@@ -96,11 +106,11 @@ int timer_get_config(unsigned long timer, unsigned char* st) {
 	unsigned char pato;
 
 	if (timer > 2){
+		printf("Error on get_config\n");
 		return 1;
 	}
 
 	// select port
-
 	if (timer == 0)
 	{
 		timer_port = TIMER_0;
@@ -122,17 +132,11 @@ int timer_get_config(unsigned long timer, unsigned char* st) {
 	timer_sel = TIMER_RB_SEL(timer);
 	input_mask |= timer_sel;
 
-	//input_mask |= TIMER_RB_COUNT_; // read counted value?
+	input_mask |= TIMER_RB_COUNT_; // read status
 
-	//input_mask |= TIMER_RB_STATUS_; // read status value?
-
-	if (sys_outb(TIMER_CTRL, input_mask)
-			|| sys_inb(timer_port,(unsigned long * ) st) != OK
-			|| sys_inb(timer_port,(unsigned long * ) &pato) != OK){
+	if (sys_outb(TIMER_CTRL, input_mask) != OK
+			|| sys_inb(timer_port,(unsigned long * ) st) != OK)
 		return 1;
-	}
-
-	printf("COUNTER: %x\n", pato);
 
 	return 0;
 }
@@ -140,25 +144,59 @@ int timer_get_config(unsigned long timer, unsigned char* st) {
 
 int timer_show_config(unsigned long timer) {
 
-	unsigned char string;
-	unsigned char* st = &string;
+	unsigned char config_output;
+	unsigned char* st = &config_output;
 	if(timer_get_config (timer,st)!=0){
-		printf("Error\n");
+		printf("Error showing config::time.c\n");
 		return 1;
 	}
 
-	printf("%x\n",*
-			st);
+	unsigned char output, nullCount, typeAcess, operatingMode, BCD;
+
+	// get info
+	output = *st >> 7;
+	nullCount = (*st >> 6) & 0x1;
+	typeAcess = (*st >> 4) & 0x3;
+	operatingMode = (*st >> 1) & 0x7;
+	BCD = *st & 0x1;
+
+	printf("Status from timer %lu: 0x%x\n\n",timer,*st);
+	printf("Output: 0x%x\n",output);
+	printf("Null Count: 0x%x\n",nullCount);
+
+	if (typeAcess == 1)
+		printf("Type Access: LSB\n");
+	else if (typeAcess == 2)
+		printf("Type Access: MSB\n");
+	else
+		printf("Type Access: LSB + MSB\n");
+
+	if (operatingMode == 0)
+		printf("Operating Mode: 0\n");
+	else if(operatingMode == 1)
+		printf("Operating Mode: 1\n");
+	else if(operatingMode == 2 || operatingMode == 6)
+		printf("Operating Mode: 2\n");
+	else if(operatingMode == 3 || operatingMode == 7)
+		printf("Operating Mode: 3\n");
+	else if(operatingMode == 4)
+		printf("Operating Mode: 4\n");
+	else
+		printf("Operating Mode: 5\n");
+
+	if (BCD == 0)
+		printf("Counting Mode: Binary (16 bits)\n");
+	else
+		printf("Counting Mode: BCD (4 digits with 4 bits each)\n");
+
 	return 0;
 }
 
 int timer_test_square(unsigned long freq) {
 
 	// test for 0
-	if (timer_set_square(0,freq))
-		return 0;
-	else
-		return 1;
+	return timer_set_square(0,freq);
+
 }
 
 int timer_test_int(unsigned long time) {
@@ -200,11 +238,10 @@ int timer_test_int(unsigned long time) {
 		}
 	}
 	timer_unsubscribe_int();
-	return 1;
+	return 0;
 }
 
 int timer_test_config(unsigned long timer) {
 
-	timer_show_config(timer);
-	return 1;
+	return timer_show_config(timer);
 }
