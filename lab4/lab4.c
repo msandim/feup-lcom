2e727af3212089f4a43f2959272c599c26fe385d
ei12013 <ei12013@979b2d34-c887-48f1-a8b5-8c534db5e80d>
2013-10-15 20:41:19 +0000

    Corrected Makefile, as it needs to use functions from timer.c Started lab4.c
    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@80 979b2d34-c887-48f1-a8b5-8c534db5e80d

---
+++ lab4/lab4.c
@@ -0,0 +1,137 @@
+#include <minix/drivers.h>
+
+#include "test4.h"
+
+static int proc_args(int argc, char *argv[]);
+static unsigned long parse_ulong(char *str, int base);
+static long parse_long(char *str, int base);
+static void print_usage(char *argv[]);
+
+int main(int argc, char **argv) {
+
+	/* Initialize service */
+	sef_startup();
+
+	if ( argc == 1 ) {
+		print_usage(argv);
+		return 0;
+	} else {
+		proc_args(argc, argv);
+	}
+	return 0;
+}
+/*
+ * int test_scan(void);
+ * int test_leds(unsigned short n, unsigned short *leds);
+ */
+
+// this function prints how to use this library
+static void print_usage(char *argv[]) {
+	printf("\n\nUsage: :\n"
+			"-   service run %s -args \"scan\" \n"
+			"    . Tests reading the scancodes from the KBC using an interrupt handler \n"
+			"-   service run %s -args \"leds <time (decimal)>\" \n"
+			"    . Tests Timer 0 interrupt handling \n"
+			argv[0], argv[0]);
+}
+
+// this function treats the args
+static int proc_args(int argc, char *argv[]) {
+
+	unsigned long freq, time, timer;
+
+	// check the function to test: if the first characters match, accept it
+
+	if (strncmp(argv[1], "square", strlen("square")) == 0) {
+		if( argc != 3 ) {
+			printf("timer.c: wrong no of arguments for test of square() \n");
+			return 1;
+		}
+
+		// get the arguments
+		if( (freq = parse_ulong(argv[2], 10)) == ULONG_MAX )
+			return 1;
+
+		timer_test_square(freq);
+
+		printf("timer.c:: square(%lu)\n\n", (unsigned) freq);
+		return 0;
+
+
+	} else if (strncmp(argv[1], "int", strlen("int")) == 0) {
+		if( argc != 3 ) {
+			printf("timer.c: wrong no of arguments for test of int() \n");
+			return 1;
+		}
+		if( (time = parse_ulong(argv[2], 10)) == ULONG_MAX )
+			return 1;
+
+		timer_test_int(time);
+
+
+		printf("timer.c:: int(%lu)\n\n", (unsigned) time);
+		return 0;
+
+	} else if (strncmp(argv[1], "config", strlen("config")) == 0) {
+		if( argc != 3 ) {
+			printf("timer.c: wrong no of arguments for test of config() \n");
+			return 1;
+		}
+		if( (timer = parse_ulong(argv[2], 10)) == ULONG_MAX )
+			return 1;
+
+		timer_test_config(timer);
+
+		printf("timer.c:: config(%lu)\n\n", (unsigned) timer);
+
+	} else {
+		printf("timer.c: non valid function \"%s\" to test\n", argv[1]);
+		return 1;
+	}
+}
+
+static unsigned long parse_ulong(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = strtoul(str, &endptr, base);
+
+	if ((errno == ERANGE && val == ULONG_MAX )
+			|| (errno != 0 && val == 0)) {
+		perror("strtol");
+		return ULONG_MAX;
+	}
+
+	if (endptr == str) {
+		printf("video_txt: parse_ulong: no digits were found in %s \n", str);
+		return ULONG_MAX;
+	}
+
+
+
+	// Successful conversion
+
+	return val;
+}
+
+static long parse_long(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = strtol(str, &endptr, base);
+
+	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+			|| (errno != 0 && val == 0)) {
+		perror("strtol");
+		return LONG_MAX;
+	}
+
+	if (endptr == str) {
+		printf("video_gr: parse_long: no digits were found in %s \n", str);
+		return LONG_MAX;
+	}
+
+	// Successful conversion
+
+	return val;
+}
