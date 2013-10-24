498791262686e40c040289ee3b6e1a0a6d2c4a0a
ei12013 <ei12013@979b2d34-c887-48f1-a8b5-8c534db5e80d>
2013-10-24 20:51:49 +0000

    Asynch function fully working
    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@115 979b2d34-c887-48f1-a8b5-8c534db5e80d

--- lab5/test5.c
+++ lab5/test5.c
@@ -68,7 +68,60 @@ int test_packet() {
 }
 
 int test_asynch(unsigned short duration) {
-	/* To be completed ... */
+	int ipc_status;
+	message msg;
+
+
+	// know what ints are we interested in
+	int irq_set_mouse = mouse_subscribe_int();
+	int irq_set_timer = timer_subscribe_int();
+
+	mouse_send_cmd(ENABLE_PACKETS);
+
+	int count = 0, timer = 0;
+
+	while( timer < duration ) {
+
+		/* Get a request message. */
+		if ( driver_receive(ANY, &msg, &ipc_status) != 0 ) {
+			printf("driver_receive failed\n");
+			continue;
+		}
+
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
+				count ++;
+
+				if (msg.NOTIFY_ARG & irq_set_mouse) { /* subscribed interrupt */
+					mouse_interrupt_handler();
+
+					if (count == 0 && ((packet[0] & bit3_mask) == bit3_mask)){
+						if (mouse_exit_handler()){
+
+						}
+					}
+				}
+				if (msg.NOTIFY_ARG & irq_set_timer) {
+					if (count%60 == 0){
+						count = 0;
+						timer ++;
+					}
+				}
+
+				break;
+			default:
+				break; /* no other notifications expected: do nothing */
+			}
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+		}
+	}
+
+	timer_unsubscribe_int();
+	mouse_unsubscribe_int();
+
+	return 0;
 }
 int test_config(void) {
 	/* To be completed ... */
