0f58949ed33e8370c19e66f33c32d864a7a9d56c
msandim <migsandim@hotmail.com>
2013-10-29 01:38:26 +0000

    lab 6 stuff added
    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@124 979b2d34-c887-48f1-a8b5-8c534db5e80d

---
+++ lab6/test6.c
@@ -0,0 +1,12 @@
+int rtc_test_conf(void) {
+	/* To be completed */
+}
+
+int rtc_test_date(void) {
+	/* To be completed */
+}
+
+int rtc_test_int(/* to be defined in class */) { 
+	/* To be completed */
+}
+
