87a373eda6647c201e0346c4356d766f6d9c5d30
msandim <migsandim@hotmail.com>
2013-10-30 00:49:56 +0000

    first function is working (I guess..)!!
    masks and "human friendly" output is pending..
    
    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@126 979b2d34-c887-48f1-a8b5-8c534db5e80d

--- lab6/test6.c
+++ lab6/test6.c
@@ -1,12 +1,97 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include <minix/com.h>
+
+#include <minix/sysutil.h>
+
+#include "test6.h"
+
+static int hook_id;
+
 int rtc_test_conf(void) {
-	/* To be completed */
+
+  unsigned long config[4];
+
+  // get the config from reg A,B,C,D
+
+  if (rtc_get_config(config))
+    return 1;
+
+  printf("A: 0x%X, B: 0x%X, C: 0x%X, D: 0x%X\n\n",
+      config[0],config[1],config[2],config[3]);
 }
 
 int rtc_test_date(void) {
-	/* To be completed */
+  /* To be completed */
 }
 
 int rtc_test_int(/* to be defined in class */) { 
-	/* To be completed */
+  /* To be completed */
 }
 
+
+int rtc_subscribe_int()
+{
+  // set hook_id with the bit to put to one
+  hook_id = RTC_IRQ;
+
+  // create a bit_mask with the int active
+  int bit_mask = BIT(hook_id);
+
+  // use both policies
+  int policy = IRQ_REENABLE | IRQ_EXCLUSIVE;
+
+  // hook_id will be changed with a different ID, but no problem
+  // because we have the bit_mask already :-)
+  if (sys_irqsetpolicy(RTC_IRQ,policy,&hook_id) != OK ||
+      sys_irqenable(&hook_id) != OK)
+    return -1;
+
+  return bit_mask;
+}
+
+
+int rtc_unsubscribe_int()
+{
+  if (sys_irqdisable(&hook_id) != OK || sys_irqrmpolicy(&hook_id) != OK)
+    return 1;
+  else
+    return 0;
+}
+
+int rtc_load_data(unsigned long addr, unsigned long *info)
+{
+  // send the address to RTC_ADDR_REG
+  if (sys_outb(RTC_ADDR_REG,addr) != OK)
+    return 1;
+
+  // receive the content of the reg
+  if (sys_inb(RTC_DATA_REG,info) != OK)
+    return 1;
+
+  return 0;
+}
+
+int rtc_save_data(unsigned long addr, unsigned long info_to_send)
+{
+  // send the address to RTC_ADDR_REG
+  if (sys_outb(RTC_ADDR_REG,addr) != OK)
+    return 1;
+
+  // send the content of the reg
+  if (sys_outb(RTC_DATA_REG,info_to_send) != OK)
+    return 1;
+
+  return 0;
+}
+
+int rtc_get_config(unsigned long config[])
+{
+  if (rtc_load_data(RTC_REG_A,&config[0]) != OK
+      || rtc_load_data(RTC_REG_B,&config[1]) != OK
+      || rtc_load_data(RTC_REG_C,&config[2]) != OK
+      || rtc_load_data(RTC_REG_D,&config[3]) != OK)
+    return 1;
+  else
+    return 0;
+}
--- lab6/test6.h
+++ lab6/test6.h
@@ -1,4 +1,48 @@
+// CONSTANTS ***********************************************************
+
+#define BIT(n) (0x01<<(n))
+
+// PORT ADDRESSES
+#define RTC_ADDR_REG 0x70
+#define RTC_DATA_REG 0x71
+
+// REG ADDRESSES
+#define RTC_REG_SECONDS 0x0
+#define RTC_REG_SECONDS_ALARM 0x1
+#define RTC_REG_MINUTES 0x2
+#define RTC_REG_MINUTES_ALARM 0x3
+#define RTC_REG_HOURS 0x4
+#define RTC_REG_HOURS_ALARM 0x5
+#define RTC_REG_WEEK_DAY 0x6
+#define RTC_REG_MONTH_DAY 0x7
+#define RTC_REG_MONTH 0x8
+#define RTC_REG_YEAR 0x9
+
+#define RTC_REG_A 0xA
+#define RTC_REG_B 0xB
+#define RTC_REG_C 0xC
+#define RTC_REG_D 0xD
+
+// CONSTANTS
+#define RTC_IRQ 0x8
+
+
+
+
+
+// TESTING FUNCTIONS ****************************************************
 int rtc_test_conf(void);
 int rtc_test_date(void);
 int rtc_test_int(/* to be defined in class */);
 
+// PROJECT FUNCTIONS ****************************************************
+
+int rtc_subscribe_int();
+
+int rtc_unsubscribe_int();
+
+int rtc_load_data(unsigned long addr, unsigned long *info);
+
+int rtc_save_data(unsigned long addr, unsigned long info_to_send);
+
+int rtc_get_config(unsigned long config[]);
