dc116f74398b6f0eb1edf7f091eaf751327ac3e8
ei12013 <ei12013@979b2d34-c887-48f1-a8b5-8c534db5e80d>
2013-11-16 23:54:39 +0000

    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@164 979b2d34-c887-48f1-a8b5-8c534db5e80d

--- lab7/test7.c
+++ lab7/test7.c
@@ -9,58 +9,59 @@
 
 int ser_test_conf(unsigned short base_addr) {
 
-  unsigned long lcr_config, ier_config, bit_rate;
-  if(ser_get_reg(base_addr,UART_LCR,&lcr_config) || ser_get_reg(base_addr,UART_IER,&ier_config))
-  {
-    printf("Error reading config\n\n");
-    return 1;
-  }
+	unsigned long lcr_config, ier_config, bit_rate;
+	if(ser_get_reg(base_addr,UART_LCR,&lcr_config) || ser_get_reg(base_addr,UART_IER,&ier_config))
+	{
+		printf("Error reading config\n\n");
+		return 1;
+	}
 
-  if (ser_get_bit_rate(base_addr,&bit_rate))
-  {
-    printf("Error reading bit rate");
-    return 1;
-  }
+	if (ser_get_bit_rate(base_addr,&bit_rate))
+	{
+		printf("Error reading bit rate");
+		return 1;
+	}
 
-  ser_show_lcr(lcr_config);
-  ser_show_ier(ier_config);
+	ser_show_lcr(lcr_config);
+	ser_show_ier(ier_config);
 
-  printf("Bite rate: %d bps\n", bit_rate);
+	printf("Bite rate: %d bps\n", bit_rate);
 
-  return 0;
+	return 0;
 }
 
 int ser_test_set(unsigned short base_addr, unsigned long bits, unsigned long stop, 
-    long parity, unsigned long rate) {
+		long parity, unsigned long rate) {
 
-  /* to be done by joao neto */
-  //printf("BASE_ADDR: %x, BITS: %x,STOP: %x,PARITY: %x, RATE: %u",base_addr,bits,stop,parity,rate);
+	ser_set_bit_rate(base_addr, rate);
+
+	printf("BASE_ADDR: %x, BITS: %x,STOP: %x,PARITY: %x, RATE: %u",base_addr,bits,stop,parity,rate);
 }
 
 int ser_test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, 
-    unsigned long stop, long parity, unsigned long rate,int stringc, char *strings[])
+		unsigned long stop, long parity, unsigned long rate,int stringc, char *strings[])
 {
-  //ser_test_set(base_addr,bits,stop,parity,rate);
+	//ser_test_set(base_addr,bits,stop,parity,rate);
 
-  //printf("STRINGC: %u, BASE_ADDR: %x,TX: %x, BITS: %x,STOP: %x,PARITY: %x, RATE: %u",stringc,base_addr,tx,bits,stop,parity,rate);
-  //printf("NUMBER OF STRS: %u, STRINGS: %s,%s",stringc,strings[0],strings[1]);
+	//printf("STRINGC: %u, BASE_ADDR: %x,TX: %x, BITS: %x,STOP: %x,PARITY: %x, RATE: %u",stringc,base_addr,tx,bits,stop,parity,rate);
+	//printf("NUMBER OF STRS: %u, STRINGS: %s,%s",stringc,strings[0],strings[1]);
 
-  unsigned int str_count;
+	unsigned int str_count;
 
-  if (tx) // if transmiter
+	if (tx) // if transmiter
 
-    // send each string!
-    for (str_count=0; str_count < stringc; str_count++)
-      ser_send_string_poll(base_addr,strings[str_count]);
+		// send each string!
+		for (str_count=0; str_count < stringc; str_count++)
+			ser_send_string_poll(base_addr,strings[str_count]);
 
-  else // if receiver
-    ser_receive_string_poll(base_addr);
+	else // if receiver
+		ser_receive_string_poll(base_addr);
 }
 
 int ser_test_int(/* details to be provided */) { 
-  /* To be completed */
+	/* To be completed */
 }
 
 int ser_test_fifo(/* details to be provided */) {
-  /* To be completed */
+	/* To be completed */
 }
