b42d98805000caf0511c21b78b2440a96ae4625b
ei12013 <ei12013@979b2d34-c887-48f1-a8b5-8c534db5e80d>
2014-01-03 03:45:13 +0000

    Documentado
    git-svn-id: https://svn.fe.up.pt/repos/lcom1314-t5g02@259 979b2d34-c887-48f1-a8b5-8c534db5e80d

--- proj/src/draw_module.h
+++ proj/src/draw_module.h
@@ -37,56 +37,196 @@
 #define MINUS2_MAKECODE 0x35
 
 
-
+//State of the drawing tools
 typedef enum {st0, st1, st2, st3} tool_state;
 
-unsigned short* getDrawScreen();
+/**
+ * @brief Returns the screen
+ *
+ * Returns the pixel array
+ *
+ * @return the pixel array in the screen
+ */
+ unsigned short* getDrawScreen();
 
-draw_screen_area getDrawScreenInfo();
+/**
+ * @brief Returns default_area
+ *
+ * Returns information about the default draw area to be used to draw the screen
+ *
+ * @return information about default draw area
+ */
+ draw_screen_area getDrawScreenInfo();
 
-BTN* getButtonArray();
+/**
+ * @brief Returns button array
+ *
+ * Returns the array of buttons in order for them to be drawn
+ *
+ * @return array of buttons
+ */
+ BTN* getButtonArray();
 
-SPRITE getColorBar();
+/**
+ * @brief Returns color bar SPRITE
+ *
+ * Returns the SPRITE representing the color bar in order for it to be drawn
+ *
+ * @return color bar SPRITE
+ */
+ SPRITE getColorBar();
 
-unsigned short getColorSelected();
+/**
+ * @brief Returns selected color
+ *
+ * Returns the selected color
+ *
+ * @return selected color
+ */
+ unsigned short getColorSelected();
+
+/**
+ * @brief Loads drawMode
+ *
+ * Loads all the information needed for the drawMode to work: draw_screen, buttons and color bar
+ *
+ * @return 0 on success, 1 if there was an error
+ */
+ int drawModeLoad();
+
+/**
+ * @brief Frees memory used by drawMode
+ *
+ * Frees memory used by the color bar, buttons and draw_screen
+ */
+ void drawModeFree();
+
+/**
+ * @brief Initialize drawMode
+ *
+ * Sets the initial values:
+ * 	tool selected to 0 (fill)
+ *	color selected to 0x2104 (black)
+ *	thickness to 5
+ *	tool state to st0
+ *	buttons to off
+ *
+ *	If the user has chosen a file from the gallery loads the file, otherwise prints a welcome message
+ *	
+ *	If the user has chosen to use the Multi mode, enables serial port
+ */
+
+ void drawModeInit(int enable_serial_com, int file_name);
 
 
-int drawModeLoad();
+/**
+ * @brief Handle mouse
+ *
+ * If the user pressed in the screen, activate selected tool
+ *
+ *	If the user pressed in the color bar, update selected color
+ */
+ void mouseDrawEvent();
 
-void drawModeFree();
+/**
+ * @brief Handle keyboard
+ *
+ * Activates tool based on received key stroke
+ *
+ * @return 0 on success, -1 if user pressed ESC to quit
+ */
+ int keyboardDrawEvent();
 
-void drawModeInit(int enable_serial_com, int file_name);
+/**
+ * @brief Checks updates on commands
+ *
+ * Loops until there are no more commands
+ */
+ void checkCommandUpdate();
 
+/**
+ * @brief Blank Handler
+ *
+ * Fills the screen with white color
+ */
+ void blank_handler();
 
-void mouseDrawEvent();
+/**
+ * @brief Brush Handler
+ *
+ * While the button is pressed, draw circles with selected thickness next to each other
+ */
+ void brush_handler();
 
-int keyboardDrawEvent();
+/**
+ * @brief Flood Fill Handler
+ *
+ * Applies the Flood Fill at the location of the mouse click
+ */
+ void flood_fill_handler();
 
-void checkCommandUpdate();
+/**
+ * @brief Color Picker Handler
+ *
+ * Sets current color to the one at the mouse location at the time of the click
+ */
+ void color_picker_handler();
 
+/**
+ * @brief Circle Handler
+ *
+ * Draws one circle receiving two mouse clicks, one at the center, and another at the extremity
+ */
+ void circle_handler();
 
-void blank_handler();
+/**
+ * @brief Rectangle Handler
+ *
+ * Draws one rectangle receiving two mouse clicks at opposite corners
+ */
+ void rectangle_handler();
 
-void brush_handler();
+/**
+ * @brief Line Handler
+ *
+ * Draws one line receiving two mouse clicks, one at each end point
+ */
+ void rect_line_handler();
 
-void flood_fill_handler();
+/**
+ * @brief Selected Area Handler
+ *
+ * Sets the draw area by defining a rectangle with two clicks at opposite corners
+ */
+ void selected_area_handler();
 
-void color_picker_handler();
+/**
+ * @brief Mirror Effect Handler
+ *
+ * Applies the mirror effect
+ */
+ void mirror_effect_handler();
 
-void circle_handler();
+/**
+ * @brief Magic Bucket Effect Handler
+ *
+ * Applies the magic bucket effect
+ */
+ void magic_bucket_handler();
 
-void rectangle_handler();
+/**
+ * @brief Date Draw Handler
+ *
+ * Stamps actual time at mouse click position in the following format
+ * HH - MM - SS DD - MM -YY
+ */
+ void date_draw_handler();
 
-void rect_line_handler();
-
-void selected_area_handler();
-
-void mirror_effect_handler();
-
-void magic_bucket_handler();
-
-void date_draw_handler();
-
-void save_handler();
+/**
+ * @brief Save Handler
+ *
+ * Saves drawing for future use, copying buffer to file
+ */
+ void save_handler();
 
 #endif
